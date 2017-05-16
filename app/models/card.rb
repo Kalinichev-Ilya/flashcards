db4e03bf62c68d2369773f3faa71f9c7b9f041d1
ilia <iluya.kalinichev@gmail.com>
2017-05-16 14:40:10 -0700

    added validations, callbacks for cards, all basic actions in card controller

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,24 @@
 class Card < ApplicationRecord
+  validates :original_text, :translated_text, presence: true
+  validate :original_text_and_translated_text_cannot_be_equals
+
+  before_create do
+    self.review_date = Time.now + (3 * 24 * 60 * 60)
+
+  end
+
+  before_validation :strip_whitespace, :only => [:original_text, :translated_text]
+
+  private
+
+  def strip_whitespace
+    self.original_text = self.original_text.strip
+    self.translated_text = self.translated_text.strip
+  end
+
+  def original_text_and_translated_text_cannot_be_equals
+    if original_text == translated_text
+      errors.add(:card, 'Original text and translated text can\'t be equals.')
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
-  get 'cards', to: 'cards#index'
 
   root 'home#index'
+  resources :cards
 end
