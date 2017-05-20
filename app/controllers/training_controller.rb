c7cbd4c3c4043e0c9ffb9117ea9ab49c3525639a
ilia <iluya.kalinichev@gmail.com>
2017-05-20 02:04:09 -0700

    fixed logic

--- app/controllers/training_controller.rb
+++ app/controllers/training_controller.rb
@@ -3,7 +3,7 @@ class TrainingController < ApplicationController
   def check
     result = CheckCard.call(user_input: params[:original_text],
                             card_id: params[:id])
-    if result.success?
+    if result
       answer = params[:original_text]
       redirect_to root_path, notice: "#{answer} Success!"
     else
